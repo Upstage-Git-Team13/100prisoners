1a6a61e18f5708346694a2ee704d9f5117515106
sususushahaha <josuhan1433@gmail.com>
2024-04-22 18:23:37 +0900

    feat: Add prison_escape_simulation

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+# 3. 전체 시뮬레이션
+# 3번 팀원 코드
+def prison_escape_simulation(boxes):
+    for prisoner_number in range(1, 101):
+        if not prisoner_attempt(prisoner_number, boxes):
+            return False  # 하나라도 실패하면 전체 실패
+    return True  # 모든 죄수가 성공
